00eb12bb4aab57071c0c995358ebfa6493bc8d76
rafaelfernandeslou <rafaelfernandeslou@gmail.com>
2025-02-25 14:06:54 -0300

    teste

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.log("Olá, Git")
-console.log("Primeiro branch")
-console.log("Segunda alteração no Branch modulo1")
+console.log("Olá, Git");
+console.log("Primeiro branch");
+console.log("Segunda alteração no Branch modulo1");
